6d985d28cb380930bdb16b1e92736f40760df7de
NIkita <nikitosgroover@gmail.com>
2022-12-11 04:56:05 +0600

    Example login/logout

--- src/components/layout/variants/left-sidebar/components/Header.tsx
+++ src/components/layout/variants/left-sidebar/components/Header.tsx
@@ -1,9 +1,17 @@
+import { useNavigate } from "react-router-dom"
+
 function Header() {
+  const navigate = useNavigate()
+  const handleLogout = () => {
+    localStorage.removeItem('token')
+    navigate('/login')
+  }
+
   return (
     <header className='h-[4rem] border-b-2 border-slate-100 flex justify-between items-center px-4'>
       <h1>Title page</h1>
       <div>
-        <button className='hover:underline'>Logout</button>
+        <button onClick={handleLogout} className='hover:underline'>Logout</button>
       </div>
     </header>
   )
--- src/pages/login/Login.tsx
+++ src/pages/login/Login.tsx
@@ -1,6 +1,12 @@
-import { Link } from 'react-router-dom'
+import { Link, useNavigate } from "react-router-dom";
 
 function Login() {
+  const navigate = useNavigate();
+  const handleLogin = () => {
+    localStorage.setItem("token", "token");
+    navigate("/");
+  };
+
   return (
     <div className="h-screen grid place-items-center bg-slate-100">
       <form className="shadow m-2 md:w-[24rem] bg-slate-300 p-4 box-border rounded">
@@ -10,11 +16,21 @@ function Login() {
           placeholder="Password"
           type="password"
         />
-        <button className="bg-blue-400 text-slate-50 px-4 py-2 rounded my-4 hover:bg-blue-500">Login</button>
-        <Link to="/registration" className="text-blue-500 px-4 py-2 rounded my-4 hover:underline">Registration</Link>
+        <button
+          onClick={handleLogin}
+          className="bg-blue-400 text-slate-50 px-4 py-2 rounded my-4 hover:bg-blue-500"
+        >
+          Login
+        </button>
+        <Link
+          to="/registration"
+          className="text-blue-500 px-4 py-2 rounded my-4 hover:underline"
+        >
+          Registration
+        </Link>
       </form>
     </div>
-  )
+  );
 }
 
-export default Login
+export default Login;
