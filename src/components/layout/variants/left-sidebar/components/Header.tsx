20f1b8c2b72d5962b3b9a2753f29f943a785ac0a
NIkita <nikitosgroover@gmail.com>
2022-12-11 04:52:25 +0600

    Base style layout

--- src/components/layout/variants/left-sidebar/components/Header.tsx
+++ src/components/layout/variants/left-sidebar/components/Header.tsx
@@ -1,9 +1,10 @@
-import React from 'react'
-
 function Header() {
   return (
-    <header className='h-[4rem] border-b-2 border-slate-100'>
-      <span>header</span>
+    <header className='h-[4rem] border-b-2 border-slate-100 flex justify-between items-center px-4'>
+      <h1>Title page</h1>
+      <div>
+        <button className='hover:underline'>Logout</button>
+      </div>
     </header>
   )
 }
--- src/components/layout/variants/left-sidebar/components/SideBar.tsx
+++ src/components/layout/variants/left-sidebar/components/SideBar.tsx
@@ -1,9 +1,14 @@
 function SideBar() {
   return (
     <aside className="h-screen bg-slate-100 overflow-y-auto">
-      <span>
-        sidebar
-      </span>
+      <div className="h-[4rem] px-4 flex items-center text-blue-400">
+        LOGO
+      </div>
+      <ul className="w-full pt-8">
+        <li className="hover:bg-slate-200 w-full p-4 cursor-pointer">Home</li>
+        <li className="hover:bg-slate-200 w-full p-4 cursor-pointer">Settings</li>
+        <li className="hover:bg-slate-200 w-full p-4 cursor-pointer">Contacts</li>
+      </ul>
     </aside>
   )
 }
